03e68f4a84a0de57677477b8c1151c2ca8ae36bd
Daniel Castillo <danielcastilloardila@hotmail.com>
2018-10-14 11:31:38 -0500

    tes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,11 @@
-import { saveUser } from "../repositories/user";
+import { saveUser, uploadImage, getImages } from "../repositories/user";
 import { createPerson } from "../services/azure";
-import { uploadImages as saveImage } from '../services/heroku';
+import { uploadImages as saveImage } from "../services/heroku";
+
 export const postUser = async (req, res) => {
   try {
-    
     let { name } = req.body;
-  
+
     //service
     let person = await createPerson(name);
     let { personId } = person;
@@ -21,11 +21,21 @@ export const postUser = async (req, res) => {
 export const uploadImages = async (req, res) => {
   try {
     let { files } = req;
-    
+    let { personId } = req.body;
     let imagesUrl = await saveImage(files);
-    res.send(imagesUrl);
+
+
+    await imagesUrl.map(image => {
+      return uploadImage({ url: image.url, personId });
+    });
+
+    let images = await getImages({personId});
+    res.send({imagenes:images});
+
 
   } catch (error) {
     console.log(error);
   }
 };
+
+
